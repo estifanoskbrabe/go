3085158f71ec470b28066c0908e327bf3dfd6548
estifanoskbrabe <estifanoskbrabe@gmail.com>
2023-10-17 12:56:16 +0300

    go

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,75 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+import java.util.Scanner;
+
+
+class Person {
+    String name;
+    int age;
+
+    void introduce() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
     }
 }
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        double num2 = 3.14;
+        char letter = 'A';
+        boolean isTrue = true;
+
+
+        int sum = num1 + 5;
+        double product = num1 * num2;
+
+        boolean isGreater = num1 > num2;
+        boolean isEqual = num1 == num2;
+
+        boolean logicalResult = isTrue && (num1 < num2 || isTrue);
+
+        if (logicalResult) {
+            System.out.println("The logical result is true.");
+        } else {
+            System.out.println("The logical result is false.");
+        }
+
+        int choice = 2;
+        switch (choice) {
+            case 1:
+                System.out.println("Option 1 selected.");
+                break;
+            case 2:
+                System.out.println("Option 2 selected.");
+                break;
+            default:
+                System.out.println("Invalid option.");
+        }
+
+
+        int count = 0;
+        while (count < 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+
+        int result = addNumbers(10, 20);
+        System.out.println("Result : " + result);
+
+
+        Person person = new Person();
+        person.name = "estifo";
+        person.age = 18;
+        person.introduce();
+    }
+
+
+    static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
+
+
